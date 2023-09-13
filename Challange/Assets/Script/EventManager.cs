6c3baab31e3f02dc586fad31c10b4e52c97664a0
DoctorRon64 <piepmail@lefthandmedia.com>
2023-09-13 16:19:45 +0200

    Eventsystem Update

---
+++ Challange/Assets/Script/EventManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EventType
+{
+    OnEnemyKilled = 0
+}
+
+public static class EventManager
+{
+    private static Dictionary<EventType, System.Action> eventDictionary = new Dictionary<EventType, System.Action>();
+
+    public static void AddListener(EventType type, System.Action action)
+    {
+        if (!eventDictionary.ContainsKey(type))
+        {
+            eventDictionary.Add(type, null);
+        }
+        eventDictionary[type] += action;
+    }
+
+    public static void RemoveListener(EventType type, System.Action action)
+    {
+        if (eventDictionary.ContainsKey(type) && eventDictionary[type] != null) { }
+        eventDictionary[type] -= action;
+    }
+
+    public static void InvokeEvent(EventType type, System.Action action)
+    {
+        eventDictionary[type]?.Invoke();
+    }
+}
--- Challange/Assets/Script/moveState.cs
+++ Challange/Assets/Script/moveState.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+using static UnityEngine.UI.Image;
 
 public class moveState : State
 {
+	[SerializeField] private GameObject Player;
 
 	public void Setup()
 	{
@@ -17,13 +21,21 @@ public class moveState : State
 
 	public override void OnUpdate()
 	{
+		/*Vector2 origin = transform.position;
+		Vector2 direction = Vector2.up;
 
+		RaycastHit2D hit;
+
+		hit = Physics2D.CircleCast(origin, 10f, direction, 10f);
+
+		if (hit.collider.GetComponent<Player>())
+		{
+			owner.switchState(Attack);
+		}*/
 	}
 
 	public override void OnExit()
 	{
 		//owner.switchState(idle)
 	}
-
-
 }
